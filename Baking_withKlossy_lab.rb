a5e780643334eef24fe3979fa2cf4f8f5a52e69d
AllyssonL <allyssonlingad@gmail.com>
2018-07-11 22:45:21 +0000

    automatically backed up by learn

--- Baking_withKlossy_lab.rb
+++ Baking_withKlossy_lab.rb
@@ -1,4 +1,4 @@
- def perfect_10_recipe = {"almond flour" => "2 cups",
+  perfect_10_recipe = {"almond flour" => "2 cups",
  "gluten freewhole oats" => "1 cup", 
  "kosher salt" => "1/2 teaspoon" ,
  "baking powder" => "1/2 teaspoon" ,
@@ -8,11 +8,11 @@
  "min dark chocolate chips" => "3/4 cup",
  "olive oil"=> "1/2 cup",
  "agave" => "1/4 cup"}
- end
+ 
 
 # recipe_ingredients["key"] = "value"
 
-# def choclate_chips
+# # def choclate_chips
 #   puts "3/4 cup"
 # end
 # choclate_chips
@@ -20,8 +20,9 @@
 # def recipe_ingredients(perfect_10_recipe)
 #   perfect_10_recipe
   
-# end
+#
 
-# def amount_of_chocolate_chips(perfect_10_recipe)
-#   perfect_10_recipe[]
+def amount_of_chocolate_chips(perfect_10_recipe)
+  return perfect_10_recipe[]
+end
   
