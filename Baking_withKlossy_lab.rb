4fbea502a9ae356c12ff0a8cc8f2c88fe3555761
AllyssonL <allyssonlingad@gmail.com>
2018-07-11 22:10:33 +0000

    automatically backed up by learn

--- Baking_withKlossy_lab.rb
+++ Baking_withKlossy_lab.rb
@@ -1,3 +1,7 @@
  recipe_ingredients = {"almond flour, gluten freewhole oats, kosher salt,baking powder, baking soda ,Xanthan Gum ,slivered almonds, lightly toasted ,min, dark chocolate chips ,olive oil,agave"}
-  
+
+end
+
+def choclate_chips
+  puts "3/4 cup"
 end
