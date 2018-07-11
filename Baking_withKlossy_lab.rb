02f57011b11a747e12d3ac9c6f340d082468f62d
AllyssonL <allyssonlingad@gmail.com>
2018-07-11 22:21:27 +0000

    automatically backed up by learn

--- Baking_withKlossy_lab.rb
+++ Baking_withKlossy_lab.rb
@@ -1,15 +1,15 @@
- recipe_ingredients = {"almond flour" => 2 cups,
- "gluten freewhole oats" => 1 cup, 
- "kosher salt" => 1/2 teaspoon ,
- "baking powder" => 1/2 teaspoon ,
- "baking soda" => 1/4 teaspoon ,
- "Xanthan Gum" => 1/2 teaspoon ,
- "slivered almonds, lightly toasted" => 1/4 cup,
- "min dark chocolate chips" => 3/4 cup ,
- "olive oil"=> 1/2 cup,
- "agave" => 1/4 cup}
+ recipe_ingredients = {"almond flour" => "2 cups",
+ "gluten freewhole oats" => "1 cup", 
+ "kosher salt" => "1/2 teaspoon" ,
+ "baking powder" => "1/2 teaspoon" ,
+ "baking soda" => "1/4 teaspoon" ,
+ "Xanthan Gum" => "1/2 teaspoon" ,
+ "slivered almonds, lightly toasted" => "1/4 cup",
+ "min dark chocolate chips" => "3/4 cup",
+ "olive oil"=> "1/2 cup",
+ "agave" => "1/4 cup"}
 
-end
+recipe_ingredients["key"] = "value"
 
 def choclate_chips
   puts "3/4 cup"
