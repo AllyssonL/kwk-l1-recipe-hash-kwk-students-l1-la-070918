6b6ebe4d5526a3018e2756e21426d12eaf960d54
AllyssonL <allyssonlingad@gmail.com>
2018-07-11 22:07:24 +0000

    automatically backed up by learn

--- Baking_withKlossy_lab.rb
+++ Baking_withKlossy_lab.rb
@@ -1,11 +1,3 @@
- recipe_ingredients = {"almond flour,gluten freewhole oats - 1 cup
-#  - kosher salt - 1/2 tea spoon
-#  - baking powder  - 1/2 tea spoon
-#  - baking soda - 1/4 tea spoon
-#  - Xanthan Gum - 1/2 tea spoon
-#  - slivered almonds, lightly toasted - 1/4 cup
-#  - mini dark chocolate chips - 3/4 cup
-#  - olive oil - 1/2 cup
-#  - agave - 1/4 cup "
+ recipe_ingredients = {"almond flour, gluten freewhole oats, kosher salt,baking powder, baking soda ,Xanthan Gum ,slivered almonds, lightly toasted ,min, dark chocolate chips ,"olive oil" ,"agave"
   
 end
